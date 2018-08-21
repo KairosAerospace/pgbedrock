d5d47805ede37a7727ec03488f2f9b307a718a54
logan <logan@hcgfunds.com>
2018-08-21 16:02:25 -0400

    Allow passwords to be retrieved for a password store.
    * pass: https://www.passwordstore.org/
    * gopass: https://www.gopass.pw/

--- pgbedrock/jinja.py
+++ pgbedrock/jinja.py
@@ -1,5 +1,11 @@
+import subprocess
+import os
+
 def do_pass(value):
-    return 'PASS SUBSTITUTION'
+    pass_path = os.environ.get('PASS_PATH', '/usr/local/bin/pass')
+
+    result = subprocess.run([pass_path, value], check=True, stdout=subprocess.PIPE)
+    return result.stdout.decode('utf8')
 
 
 def add_filters(env):
